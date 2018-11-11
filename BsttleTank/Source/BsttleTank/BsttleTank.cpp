756323257845b258bc18f4177e716919c0a6d5a3
Justin Foley <justinrfoley@yahoo.com>
2018-11-10 20:12:15 -0700

    BT04 Creating and Deleting Landscapes
    Tested creating landscapes in Unity

---
+++ BsttleTank/Source/BsttleTank/BsttleTank.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BsttleTank.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BsttleTank, "BsttleTank" );
