f57df1d2e5f557f3b17da62d9aa23a5d1b57011f
Justin Foley <justinrfoley@yahoo.com>
2018-11-11 21:32:42 -0700

    BT21 Using virtual and override
    Added code to check that we are possessing the tank when the game starts.

--- BsttleTank/Source/BsttleTank/TankPlayerController.cpp
+++ BsttleTank/Source/BsttleTank/TankPlayerController.cpp
@@ -1,10 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Public/Tank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+	ATank* ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No tank possessed"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s possessed"), *ControlledTank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
